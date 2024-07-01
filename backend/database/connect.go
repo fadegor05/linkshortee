26fcb00d2754eada5d46da55ff3205f64aa94d30
Egor Fadeev <116910468+fadegor05@users.noreply.github.com>
2024-07-01 18:56:25 +0400

    added DB var to database

--- backend/database/connect.go
+++ backend/database/connect.go
@@ -2,11 +2,14 @@ package database
 
 import (
 	"fmt"
+	"github.com/fadegor05/linkshortee/backend/models"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 	"log"
 	"os"
 )
+
+var DB *gorm.DB
 
 func Connect() *gorm.DB {
 	dsn := fmt.Sprintf("host=%s port=%s dbname=%s user=%s password=%s sslmode=disable", os.Getenv("DB_HOST"), os.Getenv("DB_PORT"), os.Getenv("DB_NAME"), os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"))
@@ -14,7 +17,10 @@ func Connect() *gorm.DB {
 	if err != nil {
 		panic(err)
 	}
-	//err = database.AutoMigrate()
+	err = database.AutoMigrate(
+		&models.Link{},
+	)
 	log.Println("Database connection established")
+	DB = database
 	return database
 }
