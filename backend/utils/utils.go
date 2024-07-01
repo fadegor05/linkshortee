af890efc4d890d65040f8cc8f25a5cd153bc2d77
Egor Fadeev <116910468+fadegor05@users.noreply.github.com>
2024-07-01 18:55:59 +0400

    fixed plain text instead json header Content-Type

--- backend/utils/utils.go
+++ backend/utils/utils.go
@@ -15,8 +15,8 @@ func ParseJSON(r *http.Request, payload any) error {
 }
 
 func WriteJSON(w http.ResponseWriter, status int, v any) error {
-	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(status)
+	w.Header().Set("Content-Type", "application/json")
 
 	return json.NewEncoder(w).Encode(v)
 }
